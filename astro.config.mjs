a5a253a2629698fe0758a20cd2380f927ae13cfb
Muca <joseclaudioprf2@gmail.com>
2026-09-07 21:15:25 -0300

    build: adiciona integracao com vercel e ajusta configs de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
-  site: 'https://SEU_USUARIO.github.io',
-  base: '/meu-portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
